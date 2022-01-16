9928e8b9d5333e11f58417450defd92662e1047b
TJ James <jamestw13@gmail.com>
2022-01-15 19:51:28 -0600

    added header structure

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      WHEN I view the footer THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn
+      profiles, and their profile on a third platform (Stack Overflow, Twitter)
+    </div>
+  );
+}
+
+export default Footer;
