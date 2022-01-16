ac2d4c0d46dc39e3d4f85c1cbfce45cccb9811db
TJ James <jamestw13@gmail.com>
2022-01-15 19:55:06 -0600

    added footer placeholders

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div>
-      WHEN I view the footer THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn
-      profiles, and their profile on a third platform (Stack Overflow, Twitter)
-    </div>
+    <footer>
+      <h4>Links</h4>
+      <ul>
+        <li>
+          <a href="github.com/jamestw13">
+            <span>logo</span>GitHub
+          </a>
+        </li>
+        <li>
+          <a href="linkedin.com/jamestw13">
+            <span>logo</span>linkedIn
+          </a>
+        </li>
+      </ul>
+      {/* WHEN I view the footer THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn
+      profiles, and their profile on a third platform (Stack Overflow, Twitter) */}
+    </footer>
   );
 }
 
