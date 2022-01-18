8f15ca805f8b24a1ed5d99a6ad8a444ed1dee07e
TJ James <jamestw13@gmail.com>
2022-01-18 09:44:45 -0600

    extracted Navigation component from header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
+import Navigation from '../Navigation';
 
 function Header() {
   return (
     <header>
       <h1>TJ James</h1>
       {/* I am presented with the developer's name  */}
-      <nav>
-        <ul>
-          <li>About Me</li>
-          <li>Portfolio</li>
-          <li>Contact</li>
-          <li>Resume</li>
-        </ul>
-      </nav>
-      {/* and navigation with titles corresponding to different sections of the portfolio  */}
-      {/* WHEN I view the navigation titles THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, */}
-      {/* and the title corresponding to the current section is highlighted WHEN I click on a navigation title THEN I am
-      presented with the corresponding section below the navigation without the page reloading and that title is
-      highlighted WHEN I load the portfolio the first time THEN the About Me title and section are selected by default */}
+      <Navigation />
     </header>
   );
 }
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>About Me</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+        <li>Resume</li>
+      </ul>
+      {/* and navigation with titles corresponding to different sections of the portfolio WHEN I view the navigation
+      titles THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding
+      to the current section is highlighted WHEN I click on a navigation title THEN I am presented with the
+      corresponding section below the navigation without the page reloading and that title is highlighted WHEN I load
+      the portfolio the first time THEN the About Me title and section are selected by default */}
+    </nav>
+  );
+}
+export default Navigation;
