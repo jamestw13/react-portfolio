2370abae3a2fb82e3b8fb9e77103c1af13be524d
TJ James <jamestw13@gmail.com>
2022-01-19 14:56:46 -0600

    extracted Navigation component from Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
-// import Navigation from '../Navigation';
+import Navigation from '../Navigation';
 
 function Header({views, currentView, setCurrentView}) {
   return (
     <header className="d-flex justify-content-between align-items-center m-3">
       <h1>TJ James</h1>
-
-      {/* I am presented with the developer's name  */}
-      {/* <Navigation /> */}
-
-      <ul className="d-flex flex-row justify-content-between">
-        {views.map(view => (
-          <li className={`p-2 alert ${(currentView === view && 'alert-primary') || 'alert-secondary'}`} key={view.name}>
-            <span onClick={() => setCurrentView(view)}>{view.name}</span>
-          </li>
-        ))}
-      </ul>
-      {/* and navigation with titles corresponding to different sections of the portfolio WHEN I view the navigation
-      titles THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding
-      to the current section is highlighted WHEN I click on a navigation title THEN I am presented with the
-      corresponding section below the navigation without the page reloading and that title is highlighted WHEN I load
-      the portfolio the first time THEN the About Me title and section are selected by default */}
+      <Navigation views={views} currentView={currentView} setCurrentView={setCurrentView} />
     </header>
   );
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,12 +4,17 @@ function Navigation({views, currentView, setCurrentView}) {
   return (
     <nav className="">
       <ul className="d-flex flex-row justify-content-between">
-        <li className="p-2">About Me</li>
-        <li className="p-2">Portfolio</li>
-        <li className="p-2">Contact</li>
-        <li className="p-2">Resume</li>
-        <li>{currentView}</li>
+        {views.map(view => (
+          <li className={`p-2 alert ${(currentView === view && 'alert-primary') || 'alert-secondary'}`} key={view.name}>
+            <span onClick={() => setCurrentView(view)}>{view.name}</span>
+          </li>
+        ))}
       </ul>
+      {/* and navigation with titles corresponding to different sections of the portfolio WHEN I view the navigation
+      titles THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding
+      to the current section is highlighted WHEN I click on a navigation title THEN I am presented with the
+      corresponding section below the navigation without the page reloading and that title is highlighted WHEN I load
+      the portfolio the first time THEN the About Me title and section are selected by default */}
       {/* and navigation with titles corresponding to different sections of the portfolio WHEN I view the navigation
       titles THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding
       to the current section is highlighted WHEN I click on a navigation title THEN I am presented with the
