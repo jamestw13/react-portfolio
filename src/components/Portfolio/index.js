74ca97064e9885e199f41a350526d32b1b37653a
TJ James <jamestw13@gmail.com>
2022-01-19 23:39:06 -0600

    added fetching of github repos and basic display

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Portfolio() {
-  return <h2>Portfolio Section</h2>;
+  const [githubData, setGithubData] = useState([{html_url: 'waiting', name: 'Loading'}]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/jamestw13/repos')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then(data => setGithubData(data))
+      .catch(error => console.log(error))
+      .finally(setLoading(false));
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <>
+      <h2>Portfolio Section</h2>
+      <div></div>
+      <ul className="list-unstyled">
+        {githubData.map(repo => (
+          <li>
+            <a href={repo.html_url}>{repo.name}</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default Portfolio;
