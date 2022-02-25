c42db8233b7735c18a4e29a35f1fca5f3b464381
jamestw13 <jamestw13@gmail.com>
2022-02-25 14:06:59 -0600

    reverted hero section, no typing animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,9 @@ function Hero() {
   return (
     <section id='hero-section'>
       <div className='spacer'></div>
-
-      <p id='about-code'>
-        {`
+      <pre>
+        <code id='about-code'>
+          {`
   about: {
     who:    "TJ James",
     what:   "Full Stack Developer",
@@ -14,8 +14,10 @@ function Hero() {
     where:  "Madison, WI,
     why:    "Better living through 1s and 0s"
   }
-            `}
-      </p>
+  
+  `}
+        </code>
+      </pre>
       <div className='spacer'></div>
     </section>
   );
